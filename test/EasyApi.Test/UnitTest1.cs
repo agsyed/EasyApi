04f7241f9b93d0ef430d7fa38d28efebbff513cd
Syed G Ali <alisyedg@gmail.com>
2022-01-27 23:05:15 -0600

    Initial Commit

---
+++ test/EasyApi.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Xunit;
+using EasyApi.Controllers;
+
+namespace EasyApi.Test;
+
+public class UnitTest1
+{
+
+    WeatherForecastController controller = new WeatherForecastController();
+
+    [Fact]
+    public void returnsWeatherForecast() 
+    {
+        var returnValue = controller.GetMockWeather();
+        Assert.True("Good".Equals(returnValue.Value));
+    }
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
